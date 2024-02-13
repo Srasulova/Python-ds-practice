195e075af921ce57fd95b05e8af46a6d6c08dd06
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-02-13 12:31:19 -0500

    finished 09 is palindrome

--- 09_is_palindrome/is_palindrome.py
+++ 09_is_palindrome/is_palindrome.py
@@ -21,3 +21,5 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
+    return phrase.replace(" ", "").lower() == phrase.replace(" ", "").lower()[::-1]
+print(is_palindrome('taco cat'))
