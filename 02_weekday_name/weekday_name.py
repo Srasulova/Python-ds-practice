9721236572b0b257be68df0893624a3f7d9ba655
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-02-13 07:38:40 -0500

    finished 02 weekday exercise

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,20 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    match day_of_week:
+        case 1:
+            return "Sunday"
+        case 2:
+            return "Monday"
+        case 3:
+            return "Tuesday"
+        case 4:
+            return "Wednesday"
+        case 5:
+            return "Thursday"
+        case 6:
+            return "Friday"
+        case 7:
+            return "Saturday"
+        case _:
+            return None
