0c1dc312d386c99e816099ef8d2d4bc74bc81d9e
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-02-14 11:49:54 -0500

    finished 17 mode

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,19 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    counts = {}
+    for item in nums:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+    max_count = max(counts.values())
+    max_keys = [key for key, value in counts.items() if value == max_count]
+    
+    return max_keys[0]
+
+
+
+
+print(mode([3, 3, 1, 2, 1, 4, 4, 4, 4, 4, 5]))
+print(mode([2, 2, 3, 3, 2, 2, 2, 2]))
